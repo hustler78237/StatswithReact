a9b66ea7f4a2b6e86047bb1ea66bbd46be2693ea
hustler78237 <160506938+hustler78237@users.noreply.github.com>
2025-07-23 12:28:56 +0530

    Add files via upload

--- Front.js
+++ Front.js
@@ -4,8 +4,8 @@ function Front() {
     return(
         <div  className="flex">
             <div className="flex flex-row justify-around items-center bg-black p-8 rounded-lg shadow-md w-full h-20 p-2 mt-6">
-            <input type="search" className="mt-10 rounded-md text-center h-10" placeholder="serach player1"/>
-            <input type="search" className="mt-10 rounded-md text-center h-10"/>
+            <input type="search" className="mx-3 rounded-md text-center h-10" placeholder="serach player1"/>
+            <input type="search" className="mx-3 rounded-md text-center h-10"/>
             </div>
         </div>
     )
