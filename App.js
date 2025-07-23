42735d10dae43505caad524338b37e15492683c2
hustler78237 <160506938+hustler78237@users.noreply.github.com>
2025-07-23 09:00:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Front from './components/Front';
+
+function App() {
+  return (
+    <div className="bg-[url('/src/images/background.png')] h-screen bg-cover bg-center">  
+         <Front/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front.js
@@ -0,0 +1,16 @@
+
+
+function Front() {
+    return(
+        <div>
+            <div className="flex flex-row justify-around">
+            <div className="">
+            <input type="search" className="mt-10 rounded-md text-center h-10" placeholder="serach player1"/>
+            <input type="search" className="mt-10 rounded-md text-center h-10"/>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Front;
