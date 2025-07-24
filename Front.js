fef1e4b8b11fde0b13eb57f4b5b4a020e6f5b4e3
hustler78237 <160506938+hustler78237@users.noreply.github.com>
2025-07-24 09:43:29 +0530

    Add files via upload

--- Front.js
+++ Front.js
@@ -2,10 +2,21 @@
 
 function Front() {
     return(
-        <div  className="flex">
-            <div className="flex flex-row justify-around items-center bg-lime-900 opacity-75 p-8 rounded-full shadow-md w-full mr-6 ml-6 h-20 p-2 mt-6">
-            <input type="search" className="my-3 rounded-md text-center h-10 opacity-100" placeholder="Search Player 1"/>
-            <input type="search" className="my-3 rounded-md text-center h-10 opacity-100" placeholder="Search Player 2"/>
+        <div  className="flex flex-col">
+            {/* this is search players */}
+            <div className="flex flex-row justify-around items-center bg-lime-900 opacity-75 rounded-full shadow-md w-[1366] mr-6 ml-6 h-20 p-2 mt-6">
+            <input type="search" className="my-3 rounded-full text-center h-10 opacity-100" placeholder="Search Player 1"/>
+            <input type="search" className="my-3 rounded-full text-center h-10 opacity-100" placeholder="Search Player 2"/>
+            </div>
+            {/* this is player images and stats */}
+            <div className="flex flex-row justify-around items-center bg-lime-900 opacity-75 rounded-lg shadow-md w-[1366] mr-6 ml-6 h-96 p-2 mt-6">
+            <input type="search" className="my-3 rounded-full text-center h-10 opacity-100" placeholder="Search Player 1"/>
+            <input type="search" className="my-3 rounded-full text-center h-10 opacity-100" placeholder="Search Player 2"/>
+            </div>
+            {/* this is conclusion of the whole stats */}
+            <div className="flex flex-row justify-around items-center bg-lime-900 opacity-75 rounded-full shadow-md w-[1366] mr-6 ml-6 h-20 p-2 mt-6">
+            <input type="search" className="my-3 rounded-full text-center h-10 opacity-100" placeholder="Search Player 1"/>
+            <input type="search" className="my-3 rounded-full text-center h-10 opacity-100" placeholder="Search Player 2"/>
             </div>
         </div>
     )
