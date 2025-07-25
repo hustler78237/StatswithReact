9dd08058e6f9b0333299d5d1e62563e652a8d96e
hustler78237 <160506938+hustler78237@users.noreply.github.com>
2025-07-25 11:25:53 +0530

    Add files via upload

--- Front.js
+++ Front.js
@@ -1,10 +1,15 @@
-
+import { useState } from "react";
 
 function Front() {
+    // this function is for select format
+    const [format, setFormat] = useState('Select Format');
+    function handleFormat(value) {
+        setFormat(value);
+    }
     return (
         <div className="flex flex-col px-4">
             {/* Search Players */}
-            <div className="flex flex-col md:flex-row justify-around items-center bg-lime-900 opacity-75 rounded-full shadow-md w-full max-w-[1366px] mx-auto h-auto md:h-20 p-4 mt-6 gap-4">
+            <div className="flex flex-col md:flex-row justify-around items-center bg-lime-900 opacity-75 rounded-lg shadow-md w-full max-w-[1366px] mx-auto h-auto md:h-20 p-4 mt-6 gap-4">
                 <input
                     type="search"
                     className="rounded-full text-center h-10 w-full md:w-1/3"
@@ -27,9 +32,19 @@ function Front() {
                         alt="Card Image"
                     />
                     <div className="p-4">
-                        <h2 className="text-xl font-semibold mb-2">Card Title</h2>
+                        <h2 className="text-xl font-semibold mb-2">Player1</h2>
+                        {/* Format changer */}
+                        <div className="relative group">
+                            <button className="bg-green-600 text-white px-6 py-2 rounded-xl cursor-pointer shadow-md" >{format}</button>
+                            <div className="absolute hidden group-hover:flex flex-row  bg-white border border-gray-300 rounded-lg shadow-lg w-48 z-20">
+                                <button className="px-4 py-2 text-left hover:bg-gray-100" onClick={() => handleFormat('ODI')}>ODI</button>
+                                <button className="px-4 py-2 text-left hover:bg-gray-100" onClick={() => handleFormat('T20')}>T20</button>
+                                <button className="px-4 py-2 text-left hover:bg-gray-100" onClick={() => handleFormat('TEST')}>TEST</button>
+                            </div>
+                        </div>
                         <p className="text-gray-700">
-                            This is some description text inside the card. You can put anything here.
+                            <h2>Batting</h2>
+                            <h2>Bowling</h2>
                         </p>
                     </div>
                 </div>
@@ -43,8 +58,18 @@ function Front() {
                     />
                     <div className="p-4">
                         <h2 className="text-xl font-semibold mb-2">Player2</h2>
+                        {/* Format changer */}
+                        <div className="relative group">
+                            <button className="bg-green-600 text-white px-6 py-2 rounded-xl cursor-pointer shadow-md" >{format}</button>
+                            <div className="absolute hidden group-hover:flex flex-row  bg-white border border-gray-300 rounded-lg shadow-lg w-48 z-20">
+                                <button className="px-4 py-2 text-left hover:bg-gray-100" onClick={() => handleFormat('ODI')}>ODI</button>
+                                <button className="px-4 py-2 text-left hover:bg-gray-100" onClick={() => handleFormat('T20')}>T20</button>
+                                <button className="px-4 py-2 text-left hover:bg-gray-100" onClick={() => handleFormat('TEST')}>TEST</button>
+                            </div>
+                        </div>
                         <p className="text-gray-700">
-                            This is some description text inside the card. You can put anything here.
+                            <h2>Batting</h2>
+                            <h2>Bowling</h2>
                         </p>
                     </div>
                 </div>
